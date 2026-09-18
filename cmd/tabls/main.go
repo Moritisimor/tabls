ff85be8499abc149e46d8ec5c9595de3e586527c
Moritisimor <devMoritisimor@proton.me>
2026-09-18 22:48:36 +0200

    Implement functionality for printing current working directory

--- cmd/tabls/main.go
+++ cmd/tabls/main.go
@@ -1,7 +1,51 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+	"time"
+
+	"github.com/fatih/color"
+	"github.com/rodaine/table"
+)
+
+func timeToString(t time.Time) string {
+	return fmt.Sprintf(
+		"%d-%d-%d %d:%d",
+		t.Year(),
+		t.Month(),
+		t.Day(),
+		t.Hour(),
+		t.Minute(),
+	)
+}
 
 func main() {
-	fmt.Println("Hello World!")
+	entries, err := os.ReadDir(".")
+	if err != nil {
+		color.Red(err.Error())
+		os.Exit(1)
+	}
+
+	tab := table.New("Name", "Type", "Size", "Modified at", "Mode")
+	headerFmt := color.New(color.FgHiGreen, color.Underline).SprintfFunc()
+  	columnFmt := color.New(color.FgHiMagenta).SprintfFunc()
+	tab.WithHeaderFormatter(headerFmt).WithFirstColumnFormatter(columnFmt)
+
+	for _, entry := range entries {
+		info, err := entry.Info()
+		if err != nil {
+			color.Red(err.Error())
+			os.Exit(1)
+		}
+
+		entryType := "File"
+		if info.IsDir() {
+			entryType = "Directory"
+		}
+
+		tab.AddRow(info.Name(), entryType, info.Size(), timeToString(info.ModTime()), info.Mode())
+	}
+
+	tab.Print()
 }
